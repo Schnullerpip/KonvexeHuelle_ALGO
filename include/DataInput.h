c43f787b34950103ca89b2bc5dc60ea4f7d0a7f1
FlorinMetzler <florinmetzler@hotmail.com>
2016-11-28 23:41:08 +0100

    Update DataInput.h
    fix

--- include/DataInput.h
+++ include/DataInput.h
@@ -6,8 +6,8 @@ void getPointsFromFile(const char* path, std::vector<Point*>* out_points);
 
 void getPointsFromRandom(std::vector<Point*>* out_points, int amount, int upper_bound, int lower_bound = 0);
 
-void DrawCirclePoints(int points, double radius, Point center, std::vector<Point*>* out_points);
+void getPointsForCircle(int points, double radius, Point center, std::vector<Point*>* out_points);
 
-void DrawSquareAround(std::vector<Point*>* out_points, int amount, int upper_bound, int lower_bound = 0);
+void getPointsForSquare(std::vector<Point*>* out_points, int amount, int upper_bound, int lower_bound = 0);
 
 
